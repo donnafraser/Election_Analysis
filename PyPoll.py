09cbf5055ec5e13de963164526cd34733b04556d
Donna Fraser <116729611+donnafraser@users.noreply.github.com>
2022-11-21 03:22:49 -0600

    Add files via upload
    I am experiencing an enormous amount of problems with my work, or actually, trying to figure out how to accomplish my work with layers of CLI, VS Code, Python Interpreter launch confusion. The issue stems from not being clear where I should have been working as I got started. Since I wasn't sure which one I should've been using, I followed the instructions as I worked through the module. Once I realized the only way any of my work would be saved was to do it in VS Code, it was too late. I worked all the way through 3.2.7 so vital info wasn't stored in my code, i.e., my_dictionary not recognized, voter_data, registered_voters, etc. the list is too long and I cant figure out how to fix it.  
    
    The Learning Assistants were great explaining I could load and use new tools, but in the end, it didn't solve any of my coding issues. So overwhelmed with just trying to figure our where files are downloading, thanks to OneDrive and additional configurations in File Explorer that work has in place, nothing is where it is 'supposed' to be following text in material, so I am not able to figure that out and actually move forward in classwork. So discouraged, but I am hoping to get it sorted out and will restart Module 3 all over again. Most likely need to buy new computer that doesn't have work stuff on it so I can start over. Haven't figure out what I should do.
    
    But I am submitting this module challenge on time so I can go back to it later and do it right. Hoping for the best.......

---
+++ PyPoll.py
@@ -0,0 +1,20 @@
+# Writing Pseudocode based on Overview of the Project list provided on 3.3.2 
+#
+# 0. Start by getting data files we need 
+# 1. Calculate total number of votes cast
+# 2. Compile a list of all candidates who received votes
+# 3. Calculate total number of votes each candidate received
+# 4. Calculate percentage of votes each candidate won
+# 5. Determine the winner of the election based on popular vote
+# 
+# from 3.3.3 instructions to write pseudocode to create a model/flowchart/roadmap for your code to be executed
+# 
+# This is the idea from the module
+
+# 1. Open the data file.
+# 2. Write down the names of all the candidates.
+# 3. Add a vote count for each candidate.
+# 4. Get the total votes for each candidate.
+# 5. Get the total votes cast for the election.
+#
+
